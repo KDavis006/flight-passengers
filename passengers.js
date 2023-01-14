75274e1980c8083f3e9e485d0b2c9043dc714966
KDavis006 <110576696+KDavis006@users.noreply.github.com>
2023-01-13 20:34:55 -0700

    idk

--- passengers.js
+++ passengers.js
@@ -19,7 +19,10 @@ class passegers{
 
 }
 
-let planeList = [];
+let plane = [
+ {}
+
+];
 
 function submit(){
  let fname = document.getElementById("fname").value
@@ -38,14 +41,13 @@ function submit(){
  let seatWScreen = document.getElementById("seatWScreen").checked
 
  let nextPassenger = new passegers(fname, lname, DoB, dateL, dateR, numBags, typeMeal, legRoom, windowSeat, extraMeal, headphones, pillowBlanket, puzzleBook, seatWScreen);
- planeList.push(nextPassenger);
- console.log(planeList);
+ plane.push(nextPassenger);
+ console.log(plane);
 }
 
-let num = -1
-
+let num = 0
 function add(){
- if(planeList[num++] != undefined){
+ if(plane[num++] != undefined){
   num++
   passengernum()
  }
@@ -57,59 +59,59 @@ function subtract(){
 }
 
 function passengernum(){
- document.getElementById("namef").value = planeList[num].firstName
- document.getElementById("namel").value = planeList[num].lastName
- document.getElementById("DoT2").value = planeList[num].DoB
- document.getElementById("dateLeave").value = planeList[num].DoTLeaving
- document.getElementById("dateReturn").value = planeList[num].DoTReturning
- document.getElementById("bagNum").value = planeList[num].numBags
- document.getElementById("typeMeal2").value = planeList[num].mealType
- document.getElementById("legRoom2").checked = planeList[num].morelegRoom
- document.getElementById("windowSeat2").checked = planeList[num].windowSeat
- document.getElementById("extraMeal2").checked = planeList[num].extraMeal
- document.getElementById("headphones2").checked = planeList[num].headphones
- document.getElementById("pillowBlanket2").checked = planeList[num].pillowBlanket
- document.getElementById("puzzleBook2").checked = planeList[num].puzzleBook
- document.getElementById("seatWScreen2").checked = planeList[num].seatWScreen
+ document.getElementById("namef").value = plane[num].firstName
+ document.getElementById("namel").value = plane[num].lastName
+ document.getElementById("DoT2").value = plane[num].DoB
+ document.getElementById("dateLeave").value = plane[num].DoTLeaving
+ document.getElementById("dateReturn").value = plane[num].DoTReturning
+ document.getElementById("bagNum").value = plane[num].numBags
+ document.getElementById("typeMeal2").value = plane[num].mealType
+ document.getElementById("legRoom2").checked = plane[num].moreLegRoom
+ document.getElementById("windowSeat2").checked = plane[num].windowSeat
+ document.getElementById("extraMeal2").checked = plane[num].extraMeal
+ document.getElementById("headphones2").checked = plane[num].headphones
+ document.getElementById("pillowBlanket2").checked = plane[num].pillowBlanket
+ document.getElementById("puzzleBook2").checked = plane[num].puzzleBook
+ document.getElementById("seatWScreen2").checked = plane[num].seatWScreen
  let costnum = 300
  if(planeList[num].windowSeat == true){
-  costnum + 10
+  costnum += 10
  }
 
  if(planeList[num].morelegRoom == true){
-  costnum + 10
+  costnum += 10
  }
   if(planeList[num].extraMeal == true){
-  costnum + 10
+  costnum += 10
  }
   if(planeList[num].headphones == true){
-  costnum + 10
+  costnum += 10
  }
   if(planeList[num].pillowBlanket == true){
-  costnum + 10
+  costnum += 10
  }
  if(planeList[num].puzzleBook == true){
-  costnum + 10
+  costnum += 10
  }
  if(planeList[num].seatWScreen == true){
-  costnum + 10
+  costnum += 10
  }
-
+ console.log(costnum);
  costnum + planeList[num].numBags * 20
  document.getElementById("cost").innerHTML = costnum
 }
 
 function edit(){
- planeList[num].firstName = document.getElementById("namef").value
- planeList[num].DoTLeaving = document.getElementById("dateLeave").value
- planeList[num].DoTReturning = document.getElementById("dateReturn").value
- planeList[num].numBags = document.getElementById("bagNum").value
- planeList[num].mealType = document.getElementById("typeMeal2").value
- planeList[num].morelegRoom = document.getElementById("legRoom2").checked
- planeList[num].windowSeat = document.getElementById("windowSeat2").checked
- planeList[num].extraMeal = document.getElementById("extraMeal2").checked
- planeList[num].headphones = document.getElementById("headphones2").checked
- planeList[num].pillowBlanket = document.getElementById("pillowBlanket2").checked 
- planeList[num].puzzleBook = document.getElementById("puzzleBook2").checked
- planeList[num].seatWScreen = document.getElementById("seatWScreen2").checked
+ plane[num].firstName = document.getElementById("namef").value
+ plane[num].DoTLeaving = document.getElementById("dateLeave").value
+ plane[num].DoTReturning = document.getElementById("dateReturn").value
+ plane[num].numBags = document.getElementById("bagNum").value
+ plane[num].mealType = document.getElementById("typeMeal2").value
+ plane[num].morelegRoom = document.getElementById("legRoom2").checked
+ plane[num].windowSeat = document.getElementById("windowSeat2").checked
+ plane[num].extraMeal = document.getElementById("extraMeal2").checked
+ plane[num].headphones = document.getElementById("headphones2").checked
+ plane[num].pillowBlanket = document.getElementById("pillowBlanket2").checked 
+ plane[num].puzzleBook = document.getElementById("puzzleBook2").checked
+ plane[num].seatWScreen = document.getElementById("seatWScreen2").checked
 }

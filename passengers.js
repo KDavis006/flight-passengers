73bc23526998d3828053c002fb20c361046aa90c
KDavis006 <110576696+KDavis006@users.noreply.github.com>
2023-01-13 18:26:07 -0700

    changes

--- passengers.js
+++ passengers.js
@@ -41,3 +41,75 @@ function submit(){
  planeList.push(nextPassenger);
  console.log(planeList);
 }
+
+let num = -1
+
+function add(){
+ if(planeList[num++] != undefined){
+  num++
+  passengernum()
+ }
+}
+
+function subtract(){
+ num--
+ passengernum()
+}
+
+function passengernum(){
+ document.getElementById("namef").value = planeList[num].firstName
+ document.getElementById("namel").value = planeList[num].lastName
+ document.getElementById("DoT2").value = planeList[num].DoB
+ document.getElementById("dateLeave").value = planeList[num].DoTLeaving
+ document.getElementById("dateReturn").value = planeList[num].DoTReturning
+ document.getElementById("bagNum").value = planeList[num].numBags
+ document.getElementById("typeMeal2").value = planeList[num].mealType
+ document.getElementById("legRoom2").checked = planeList[num].morelegRoom
+ document.getElementById("windowSeat2").checked = planeList[num].windowSeat
+ document.getElementById("extraMeal2").checked = planeList[num].extraMeal
+ document.getElementById("headphones2").checked = planeList[num].headphones
+ document.getElementById("pillowBlanket2").checked = planeList[num].pillowBlanket
+ document.getElementById("puzzleBook2").checked = planeList[num].puzzleBook
+ document.getElementById("seatWScreen2").checked = planeList[num].seatWScreen
+ let costnum = 300
+ if(planeList[num].windowSeat == true){
+  costnum + 10
+ }
+
+ if(planeList[num].morelegRoom == true){
+  costnum + 10
+ }
+  if(planeList[num].extraMeal == true){
+  costnum + 10
+ }
+  if(planeList[num].headphones == true){
+  costnum + 10
+ }
+  if(planeList[num].pillowBlanket == true){
+  costnum + 10
+ }
+ if(planeList[num].puzzleBook == true){
+  costnum + 10
+ }
+ if(planeList[num].seatWScreen == true){
+  costnum + 10
+ }
+
+ costnum + planeList[num].numBags * 20
+ document.getElementById("cost").innerHTML = costnum
+}
+
+function edit(){
+ planeList[num].firstName = document.getElementById("namef").value
+ planeList[num].DoTLeaving = document.getElementById("dateLeave").value
+ planeList[num].DoTReturning = document.getElementById("dateReturn").value
+ planeList[num].numBags = document.getElementById("bagNum").value
+ planeList[num].mealType = document.getElementById("typeMeal2").value
+ planeList[num].morelegRoom = document.getElementById("legRoom2").checked
+ planeList[num].windowSeat = document.getElementById("windowSeat2").checked
+ planeList[num].extraMeal = document.getElementById("extraMeal2").checked
+ planeList[num].headphones = document.getElementById("headphones2").checked
+ planeList[num].pillowBlanket = document.getElementById("pillowBlanket2").checked 
+ planeList[num].puzzleBook = document.getElementById("puzzleBook2").checked
+ planeList[num].seatWScreen = document.getElementById("seatWScreen2").checked
+}
